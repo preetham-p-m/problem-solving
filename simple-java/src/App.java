bec9003a964570cf44d722d518034069c933220e
preetham-p-m <preethampm1@gmail.com>
2024-09-03 23:32:16 +0530

    topological sort

--- simple-java/src/App.java
+++ simple-java/src/App.java
@@ -1,5 +1,103 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Stack;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int V = 4;
+
+        // Edges
+        List<List<Integer>> edges = new ArrayList<>();
+        edges.add(Arrays.asList(0, 1));
+        edges.add(Arrays.asList(1, 2));
+        edges.add(Arrays.asList(3, 1));
+        edges.add(Arrays.asList(3, 2));
+
+        // Graph represented as an adjacency list
+        List<List<Integer>> adj = new ArrayList<>(V);
+        for (int i = 0; i < V; i++) {
+            adj.add(new ArrayList<>());
+        }
+
+        for (List<Integer> i : edges) {
+            adj.get(i.get(0)).add(i.get(1));
+        }
+
+        System.out.println(edges);
+        System.out.println(adj);
+    }
+
+    static void dfs(int node, ArrayList<ArrayList<Integer>> adj, boolean[] visited, Stack<Integer> stack) {
+        visited[node] = true;
+
+        for (int i : adj.get(node)) {
+            if (!visited[i]) {
+                dfs(i, adj, visited, stack);
+            }
+        }
+
+        stack.push(node);
+    }
+
+    static int[] topoSort(int V, ArrayList<ArrayList<Integer>> adj) {
+        var stack = new Stack<Integer>();
+        var visited = new boolean[V];
+
+        for (int i = 0; i < V; i++) {
+            if (!visited[i]) {
+                dfs(i, adj, visited, stack);
+            }
+        }
+
+        List<Integer> result = new ArrayList<Integer>();
+        while (!stack.isEmpty()) {
+            result.add(stack.pop());
+        }
+
+        return result.stream().mapToInt(i -> i).toArray();
+    }
+
+    static int[] topoSortUsingBSF(int V, ArrayList<ArrayList<Integer>> adj) {
+
+        int[] inDegree = new int[V];
+
+        for (int i = 0; i < V; i++) {
+            for (int it : adj.get(i)) {
+                inDegree[it]++;
+            }
+        }
+
+        Queue<Integer> queue = new LinkedList<Integer>();
+        for (int i = 0; i < V; i++) {
+            if (inDegree[i] == 0) {
+                queue.add(i);
+            }
+        }
+
+        int[] result = new int[V];
+        int index = 0;
+
+        while (!queue.isEmpty()) {
+            var node = queue.remove();
+            result[index++] = node;
+
+            for (int it : adj.get(node)) {
+                inDegree[it]--;
+
+                if (inDegree[it] == 0) {
+                    queue.add(it);
+                }
+            }
+        }
+
+        if (index != V) {
+            System.out.println("Graph contains cycle!");
+            return new int[0];
+        }
+
+        return result;
     }
 }
