b5f7fc05dfe42c08c63d9de69dc5274b80a87709
preetham-p-m <preethampm1@gmail.com>
2024-09-19 10:41:00 +0530

    unbounded knapsack

---
+++ simple-py/dp/knapsack_unbounded.py
@@ -0,0 +1,17 @@
+from typing import List
+
+
+def knapsack_unbounded(weights: List[int], values: List[int], W: int):
+
+    N = len(values)
+
+    dp = [[0 for _ in range(W + 1)] for _ in range(N + 1)]
+
+    for n in range(1, N + 1):
+        for w in range(1, W + 1):
+            if weights[n - 1] <= w:
+                dp[n][w] = max(dp[n - 1][w], values[n - 1] + dp[n][w - weights[n - 1]])
+            else:
+                dp[n][w] = dp[n - 1][w]
+
+    return dp[N][W]
