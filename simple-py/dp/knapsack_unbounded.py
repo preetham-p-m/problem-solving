ca11a9d336133bafbc5cbff7f50f538179521ae1
preetham-p-m <preethampm1@gmail.com>
2024-09-20 07:57:46 +0530

    update

--- simple-py/dp/knapsack_unbounded.py
+++ simple-py/dp/knapsack_unbounded.py
@@ -1,16 +1,16 @@
 from typing import List
 
 
-def knapsack_unbounded(weights: List[int], values: List[int], W: int):
+def knapsack_unbounded(weights: List[int], profit: List[int], W: int):
 
-    N = len(values)
+    N = len(profit)
 
     dp = [[0 for _ in range(W + 1)] for _ in range(N + 1)]
 
     for n in range(1, N + 1):
         for w in range(1, W + 1):
             if weights[n - 1] <= w:
-                dp[n][w] = max(dp[n - 1][w], values[n - 1] + dp[n][w - weights[n - 1]])
+                dp[n][w] = max(dp[n - 1][w], profit[n - 1] + dp[n][w - weights[n - 1]])
             else:
                 dp[n][w] = dp[n - 1][w]
 
