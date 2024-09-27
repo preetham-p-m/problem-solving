81e077776e4e8cc5243b0d109bc229db86480fe9
preetham-p-m <preethampm1@gmail.com>
2024-09-27 19:35:28 +0530

    minimum insertion to make the string palindrome

---
+++ simple-py/dp/strings/minimum_insertion.py
@@ -0,0 +1,15 @@
+def minInsertions(s: str) -> int:
+    s2 = s[::-1]
+
+    N = len(s)
+
+    dp = [[0 for _ in range(N + 1)] for _ in range(N + 1)]
+
+    for n in range(1, N + 1):
+        for m in range(1, N + 1):
+            if s[n - 1] == s2[m - 1]:
+                dp[n][m] = 1 + dp[n - 1][m - 1]
+            else:
+                dp[n][m] = max(dp[n - 1][m], dp[n][m - 1])
+
+    return N - dp[N][N]
