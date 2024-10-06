887074d35f981360a3a7838a8829b61a95848c4d
preetham-p-m <preethampm1@gmail.com>
2024-10-06 15:08:07 +0530

    update super egg break

--- simple-py/dp/strings/super_egg_drop.py
+++ simple-py/dp/strings/super_egg_drop.py
@@ -2,26 +2,36 @@ from typing import List
 
 
 def superEggDrop(k: int, n: int) -> int:
-
     def egg_drop(k: int, n: int, dp: List[List[int]]) -> int:
-
+        # Base cases
         if n == 0 or n == 1:
             return n
-
         if k == 1:
             return n
 
         if dp[n][k] != -1:
             return dp[n][k]
 
-        minimum: int = float("infinity")
+        low, high = 1, n
+        minimum = float("infinity")
 
-        for a in range(1, n):
-            temp: int = 1 + max(egg_drop(k - 1, a - 1, dp), egg_drop(k, n - a, dp))
-            minimum = min(minimum, temp)
+        while low <= high:
+            mid = (low + high) // 2
+            egg_breaks = egg_drop(k - 1, mid - 1, dp)
+            egg_doesnt_break = egg_drop(k, n - mid, dp)
+
+            worst_case = max(egg_breaks, egg_doesnt_break) + 1
+
+            minimum = min(minimum, worst_case)
+
+            if egg_breaks > egg_doesnt_break:
+                high = mid - 1
+            else:
+                low = mid + 1
 
         dp[n][k] = minimum
         return minimum
 
     dp = [[-1 for _ in range(k + 1)] for _ in range(n + 1)]
+
     return egg_drop(k, n, dp)
