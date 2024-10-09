a104290d6c8e07ebc7c524549feca9a475832191
preetham-p-m <preethampm1@gmail.com>
2024-10-09 10:08:49 +0530

    Number of islands

---
+++ simple-java/src/graphs/NumberOfIslands.java
@@ -0,0 +1,64 @@
+package graphs;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class NumberOfIslands {
+
+    class Pair {
+        int first;
+        int second;
+
+        public Pair(int first, int second) {
+            this.first = first;
+            this.second = second;
+        }
+    }
+
+    private final int[] rowDir = { -1, 1, 0, 0 };
+    private final int[] colDir = { 0, 0, -1, 1 };
+
+    private void bfs(char[][] grid, int row, int col, boolean[][] visited) {
+        int rowLength = grid.length, colLength = grid[0].length;
+        visited[row][col] = true;
+
+        Queue<Pair> queue = new LinkedList<>();
+        queue.add(new Pair(row, col));
+
+        while (!queue.isEmpty()) {
+            int currentRow = queue.peek().first;
+            int currentCol = queue.peek().second;
+            queue.remove();
+
+            for (int i = 0; i < 4; i++) {
+                int effRow = currentRow + rowDir[i];
+                int effCol = currentCol + colDir[i];
+
+                if (effRow >= 0 && effRow < rowLength && effCol >= 0 && effCol < colLength
+                        && grid[effRow][effCol] == '1' && !visited[effRow][effCol]) {
+                    visited[effRow][effCol] = true;
+                    queue.add(new Pair(effRow, effCol));
+                }
+
+            }
+
+        }
+    }
+
+    public int numIslands(char[][] grid) {
+        int islands = 0;
+        int rowLength = grid.length, colLength = grid[0].length;
+        boolean[][] visited = new boolean[rowLength][colLength];
+
+        for (int row = 0; row < rowLength; row++) {
+            for (int col = 0; col < colLength; col++) {
+                if (!visited[row][col] && grid[row][col] == '1') {
+                    bfs(grid, row, col, visited);
+                    islands++;
+                }
+            }
+        }
+
+        return islands;
+    }
+}
