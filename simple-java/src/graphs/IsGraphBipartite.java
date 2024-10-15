7d6037e980821b102125086f5944a03d28036c29
preetham-p-m <preethampm1@gmail.com>
2024-10-15 09:20:52 +0530

    added Is Graph Bipartite?

---
+++ simple-java/src/graphs/IsGraphBipartite.java
@@ -0,0 +1,44 @@
+package graphs;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class IsGraphBipartite {
+
+    private boolean checkIsBipartite(int[][] graph, int[] bipartite, int start) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(start);
+        bipartite[start] = 1;
+
+        while (!queue.isEmpty()) {
+            int node = queue.poll();
+
+            for (int i : graph[node]) {
+                if (bipartite[i] == 0) {
+                    bipartite[i] = 3 - bipartite[node];
+                    queue.add(i);
+                } else if (bipartite[i] == bipartite[node]) {
+                    return false;
+                }
+            }
+
+        }
+
+        return true;
+    }
+
+    public boolean isBipartite(int[][] graph) {
+        int N = graph.length;
+        int[] bipartite = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            if (bipartite[i] == 0) {
+                if (!checkIsBipartite(graph, bipartite, i)) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
